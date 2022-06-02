7255f100169ebcc83c7e4cc0941e94baa641b5d3
chending <chending_6202320@126.com>
2022-06-02 10:29:10 +0800

    fix: 合并cd分支

--- APILib/rbklib.py
+++ APILib/rbklib.py
@@ -8,7 +8,8 @@ from typing import Any, Union
 
 PACK_FMT_STR = '!BBHLH6s'
 
-def getIP()->str:
+
+def getIP() -> str:
     """获取机器人ip
 
     Returns:
@@ -19,7 +20,8 @@ def getIP()->str:
     config_path = os.path.join(p, "config.json")
     with open(config_path, "r") as f:
         j = json.load(f)
-        return j.get("rbk_ip","")
+        return j.get("rbk_ip", "")
+
 
 def packMasg(reqId, msgType, msg={}):
     msgLen = 0
@@ -30,17 +32,18 @@ def packMasg(reqId, msgType, msg={}):
         jsonStr = msg
     if (msg != {}):
         msgLen = len(jsonStr)
-    print(msgLen,"***************************")
-    rawMsg = struct.pack(PACK_FMT_STR, 0x5A, 0x01, reqId, msgLen,msgType, b'\x00\x00\x00\x00\x00\x00')
+    print(msgLen, "***************************")
+    rawMsg = struct.pack(PACK_FMT_STR, 0x5A, 0x01, reqId, msgLen, msgType, b'\x00\x00\x00\x00\x00\x00')
     # print("{:02X} {:02X} {:04X} {:08X} {:04X}"
     # .format(0x5A, 0x01, reqId, msgLen, msgType))
 
     if (msg != {}):
-        rawMsg += bytearray(jsonStr,'ascii')
+        rawMsg += bytearray(jsonStr, 'ascii')
         # print(msg)
 
     return rawMsg
-    
+
+
 def normalize_theta(theta):
     if theta >= -math.pi and theta < math.pi:
         return theta
@@ -54,9 +57,10 @@ def normalize_theta(theta):
 
 
 class rbklib:
-    def __init__(self, ip, core_flag = False) -> None:
+    def __init__(self, ip, core_flag=False) -> None:
         self.ip = ip
         try:
+            # 机器人状态 socket
             self.so_19204 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.so_19204.connect((self.ip, 19204))
             self.so_19204.settimeout(5)
@@ -65,61 +69,52 @@ class rbklib:
         print("core_flag", core_flag)
         if core_flag == False:
             try:
+                # 机器人控制 socket
                 self.so_19205 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.so_19205.connect((self.ip, 19205))
                 self.so_19205.settimeout(5)
             except:
                 self.so_19205 = None
             try:
+                # 机器人导航 socket
                 self.so_19206 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.so_19206.connect((self.ip, 19206))
                 self.so_19206.settimeout(5)
             except:
-                self.so_19205 = None
+                self.so_19206 = None
         else:
             self.so_19205 = None
             self.so_19206 = None
         try:
+            # 机器人配置 socket
             self.so_19207 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.so_19207.connect((self.ip, 19207))
             self.so_19207.settimeout(5)
         except:
             self.so_19207 = None
-
-    def __del__(self):
-        if self.so_19204 != None:
-            self.so_19204.close()
-        if self.so_19207 != None:
-            self.so_19207.close()
-        if self.so_19205 != None:
-            self.so_19205.close()
-        if self.so_19206 != None:
-            self.so_19206.close()
-        # 机器人状态 socket
-        self.so_19204 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.so_19204.connect((self.ip, 19204))
-        self.so_19204.settimeout(5)
-        if not core_flag:
-            # 机器人控制 socket
-            self.so_19205 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.so_19205.connect((self.ip, 19205))
-            self.so_19205.settimeout(5)
-            # 机器人导航 socket
-            self.so_19206 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.so_19206.connect((self.ip, 19206))
-            self.so_19206.settimeout(5)
-        # 机器人配置 socket
-        self.so_19207 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.so_19207.connect((self.ip, 19207))
-        self.so_19207.settimeout(5)
-        # 其他 socket
-        self.so_19210 = socket.socket()
-        self.so_19210.connect((self.ip, 19210))
-        self.so_19210.settimeout(5)
+        try:
+            # 其他 socket
+            self.so_19210 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.so_19210.connect((self.ip, 19210))
+            self.so_19210.settimeout(5)
+        except:
+            self.so_19210 = None
         # # 机器人推送 socket
         # self.so_19301 = socket.socket()
         # self.so_19301.connect((self.ip, 19301))
         # self.so_19301.settimeout(5)
+
+    def __del__(self):
+        if self.so_19204 != None:
+            self.so_19204.close()
+        if self.so_19205 != None:
+            self.so_19205.close()
+        if self.so_19206 != None:
+            self.so_19206.close()
+        if self.so_19207 != None:
+            self.so_19207.close()
+        if self.so_19210 != None:
+            self.so_19210.close()
 
     def query(self, msg):
         so = self.so_19204
@@ -132,7 +127,7 @@ class rbklib:
             print('timeout')
         jsonDataLen = 0
         backReqNum = 0
-        if(len(data) < 16):
+        if (len(data) < 16):
             print('pack head error')
             print(data)
         else:
@@ -159,7 +154,7 @@ class rbklib:
             print('timeout')
         return jdata
 
-    def getResponse(self, so:socket.socket):
+    def getResponse(self, so: socket.socket):
         dataall = b''
         try:
             data = so.recv(16)
@@ -167,15 +162,15 @@ class rbklib:
             print('timeout')
         jsonDataLen = 0
         backReqNum = 0
-        if(len(data) < 16):
+        if (len(data) < 16):
             print('pack head error')
             print(data)
         else:
             header = struct.unpack(PACK_FMT_STR, data)
             print("{:02X} {:02X} {:04X} {:08X} {:04X} {:02X} {:02X} {:02X} {:02X} {:02X} {:02X}       length: {}"
-            .format(header[0], header[1], header[2], header[3], header[4],
-            header[5][0], header[5][1], header[5][2], header[5][3], header[5][4], header[5][5],
-            header[3]))
+                  .format(header[0], header[1], header[2], header[3], header[4],
+                          header[5][0], header[5][1], header[5][2], header[5][3], header[5][4], header[5][5],
+                          header[3]))
             jsonDataLen = header[3]
             backReqNum = header[4]
         dataall += data
@@ -197,19 +192,19 @@ class rbklib:
     def reloc(self, pos):
         so = self.so_19205
         print("pos:", pos)
-        data = {"x":pos["x"], "y":pos["y"], "angle":pos["angle"]/180.0*math.pi}
-        msg = packMasg(1,2002, data)
+        data = {"x": pos["x"], "y": pos["y"], "angle": pos["angle"] / 180.0 * math.pi}
+        msg = packMasg(1, 2002, data)
         so.send(msg)
         self.getResponse(so)
 
     def confirmLoc(self):
         so = self.so_19205
-        msg = packMasg(1,2003)
+        msg = packMasg(1, 2003)
         so.send(msg)
 
     def cancelLoc(self):
         so = self.so_19205
-        msg = packMasg(1,2004)
+        msg = packMasg(1, 2004)
         so.send(msg)
 
     def moveLoc(self, pos):
@@ -219,21 +214,20 @@ class rbklib:
         self.cancelLoc()
         time.sleep(1)
         self.confirmLoc()
-    
-    def moveRobot(self, pos:dict):
-        sim_pos ={"sim":{"setPos":pos}}
+
+    def moveRobot(self, pos: dict):
+        sim_pos = {"sim": {"setPos": pos}}
         self.sendTask(sim_pos)
-        self.reloc(pos = pos)
+        self.reloc(pos=pos)
         time.sleep(1)
         self.cancelLoc()
         time.sleep(1)
         self.confirmLoc()
 
-
     def lock(self):
         so = self.so_19207
-        data = {"nick_name":"rbklib"}
-        msg = packMasg(1,4005, data)
+        data = {"nick_name": "rbklib"}
+        msg = packMasg(1, 4005, data)
         so.send(msg)
 
     # def uploadMap(self, map_name:str):
@@ -253,56 +247,57 @@ class rbklib:
 
     def clearErrors(self):
         so = self.so_19207
-        msg = packMasg(1,4009)
+        msg = packMasg(1, 4009)
         so.send(msg)
 
     def cancelTask(self):
         so = self.so_19206
-        msg = packMasg(1,3003)
+        msg = packMasg(1, 3003)
         so.send(msg)
 
     def getPos(self):
-        pos_msg = packMasg(1,1004)
+        pos_msg = packMasg(1, 1004)
         data = self.query(pos_msg)
         return data
 
     def getVel(self):
-        pos_msg = packMasg(1,1005)
+        pos_msg = packMasg(1, 1005)
         data = self.query(pos_msg)
         return data
 
     def getTaskStatus(self):
-        msg = packMasg(1,1020)
+        msg = packMasg(1, 1020)
         data = self.query(msg)
         return data
 
     def getRobotStatus(self):
-        msg = packMasg(1,1000)
+        msg = packMasg(1, 1000)
         data = self.query(msg)
         return data
 
     def getError(self):
-        msg = packMasg(1,1050)
+        msg = packMasg(1, 1050)
         data = self.query(msg)
-        return data        
+        return data
 
     def getStatusInfo(self):
         msg = packMasg(1, 1000)
         data = self.query(msg)
         return data
-        
+
     def sendTask(self, data):
         so = self.so_19206
-        msg = packMasg(1,3051,data)
+        msg = packMasg(1, 3051, data)
         so.send(msg)
 
     def sendTaskList(self, data):
         so = self.so_19206
         task = {"move_task_list": data}
         print(task)
-        msg = packMasg(1,3066,task)
+        msg = packMasg(1, 3066, task)
         so.send(msg)
-    def modifyParam(self, data:dict()):
+
+    def modifyParam(self, data: dict()):
         """永久修改参数配置里面的参数
 
         Args:
@@ -314,19 +309,19 @@ class rbklib:
             }
         """
         so = self.so_19207
-        msg = packMasg(1,4101, data)
-        so.send(msg)     
-    
+        msg = packMasg(1, 4101, data)
+        so.send(msg)
+
     def recoveryParam(self):
         data = []
         so = self.so_19207
-        msg = packMasg(1,4102, data)
-        so.send(msg)               
+        msg = packMasg(1, 4102, data)
+        so.send(msg)
 
     def translate(self, data):
         """机器人平动"""
         so = self.so_19206
-        msg = packMasg(1,3055,data)
+        msg = packMasg(1, 3055, data)
         so.send(msg)
 
     def request(self, msgType: int, reqId: int = 1, msg: Union[dict[str, Any], list, bytearray, bytes] = None,
@@ -1665,7 +1660,7 @@ class rbklib:
         return self.request(6083)
 
     def robot_other_slam_req(self, real_time: bool = None, screen_width: float = None, screen_height: float = None) -> \
-    tuple[tuple, bytes]:
+            tuple[tuple, bytes]:
         """
         开始扫描地图\n
         如果要接受实时的扫图数据, 请监听 UDP 19301 端口, 数据格式为二进制的 rbk.protocol.Message_MapLog\n
