eb43118944d7a1fd933f91f78504412dcc04d0f6
向英杰 <xiang.yingjie@qq.com>
2022-07-11 21:25:43 +0800

    add: 威普特 v3

--- test_rdscore/test_weipute/test_weipute.py
+++ test_rdscore/test_weipute/test_weipute.py
@@ -245,8 +245,19 @@ def test_19():
     o1 = ORDER.gotoOrder(vehicle="AMB-01",location="AP28")
     o2 = ORDER.gotoOrder(vehicle="AMB-02",location="AP45")
 
+def test_20():
+    """ 
+    """
+    ORDER.terminateAll(vehicle = ["AMB-01", "AMB-02"])
+    time.sleep(2)
+    init_pos(loc = "LM147", name="AMB-01", a = 1.57)
+    init_pos(loc = "CP134", name="AMB-02", a = -1.57)
+    ORDER.dispatchable(name = ["AMB-01", "AMB-02"])
+    o1 = ORDER.gotoOrder(vehicle="AMB-01",location="CP134")
+    o2 = ORDER.gotoOrder(vehicle="AMB-02",location="PP50")
+
 if __name__ == "__main__":
     # maybe fail: 
     # 3
-    test_3()
+    test_20()
 
