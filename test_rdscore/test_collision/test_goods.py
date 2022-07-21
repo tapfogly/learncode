9b9354eb3497cbc9adfbf68e8aab84180f3a33f2
yda <yangda@seer-group.com>
2022-07-21 14:51:08 +0800

    robokit#1213 feat: 更多的测试用例

--- test_rdscore/test_collision/test_goods.py
+++ test_rdscore/test_collision/test_goods.py
@@ -7,12 +7,14 @@ from APILib.orderLib import *
 
 core = OrderLib(getServerAddr())
 
+
 def setup_module():
     """执行这个脚本的用例前需要的准备内容
     """
-    core.modifyParam({"RDSDispatcher":{"AutoPark":False}})
-    core.uploadScene(Path(Path(__file__).absolute().parent,"lvyuan.zip"))
-    time.sleep(60) # 更新很慢
+    # core.modifyParam({"RDSDispatcher":{"AutoPark":False}})
+    # core.uploadScene(Path(Path(__file__).absolute().parent,"lvyuan.zip"))
+    # time.sleep(60) # 更新很慢
+
 
 def set_sim_pos_by_name(vehicle, pos_name):
     core.updateSimRobotState(json.dumps({"vehicle_id": vehicle, "position_by_name": pos_name}))
@@ -48,7 +50,60 @@ def test_goods_collision():
     set_sim_goods_shape(v2, large_goods_box)
     time.sleep(1)
 
+    # set_sim_goods_vehicle_angle(v2, -1.57)
+
+    order_id1 = core.gotoOrder(vehicle=v1, location=target1)
+    order_id2 = core.gotoOrder(vehicle=v2, location=target2)
+
+    core.waitForOrderFinish(order_id1)
+    core.waitForOrderFinish(order_id2)
+
+
+def test_goods_collision2():
+    """车不动，旋转货物
+    """
+    core.terminateAll([])
+    time.sleep(3)
+    core.dispatchable(v1)
+    core.dispatchable(v2)
+    time.sleep(3)
+
+    set_sim_pos_by_name(v1, p1)
+    set_sim_pos_by_name(v2, p2)
+    # 设置货物
+    set_sim_goods_shape(v1, large_goods_box)
+    set_sim_goods_shape(v2, large_goods_box)
+    time.sleep(1)
+
+    set_sim_goods_vehicle_angle(v2, 0)
+    time.sleep(2)
+
+    order_id1 = core.gotoOrder(vehicle=v1, location=target1)
+    order_id2 = core.gotoOrder(vehicle=v2, location=target2)
+
+    core.waitForOrderFinish(order_id1)
+    core.waitForOrderFinish(order_id2)
+
+
+def test_goods_collision3():
+    """车不动，旋转货物
+    """
+    core.terminateAll([])
+    time.sleep(3)
+    core.dispatchable(v1)
+    core.dispatchable(v2)
+    time.sleep(3)
+
+    set_sim_pos_by_name(v1, p1)
+    set_sim_pos_by_name(v2, p2)
+    # 设置货物
+    set_sim_goods_shape(v1, large_goods_box)
+    set_sim_goods_shape(v2, large_goods_box)
+    time.sleep(1)
+
     set_sim_goods_vehicle_angle(v2, -1.57)
+    set_sim_goods_vehicle_angle(v1, -1.57)
+    time.sleep(2)
 
     order_id1 = core.gotoOrder(vehicle=v1, location=target1)
     order_id2 = core.gotoOrder(vehicle=v2, location=target2)
